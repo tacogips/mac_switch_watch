53932f7a39966df4c61ef224fd97afae34678f05
kawada <kawada@nebloe.org>
2015-09-06 19:33:55 +0900

    add example

---
+++ example/example.go
@@ -0,0 +1,18 @@
+package main
+
+/* example: output comment to console on sleep/wake up */
+import "github.com/kuxuxun/mac_switch_watch"
+
+func main() {
+	mac_switch_watch.SetHandler(mac_switch_watch.KeyOnSystemWillSleep, OnSleep)
+	mac_switch_watch.SetHandler(mac_switch_watch.KeyOnSystemWillPowerOn, OnWake)
+
+	mac_switch_watch.Watch()
+}
+func OnSleep() {
+	println("sleep!!")
+}
+
+func OnWake() {
+	println("wake!!")
+}
